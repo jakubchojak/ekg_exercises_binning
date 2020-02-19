cf6d9f7cb718fcbd573fefe912718fa5cebe21cf
Jakub Chojak <abukkajohc@gmail.com>
2020-02-19 21:36:24 +0100

    counting average and finding min/max value working

--- ekg_python.py
+++ ekg_python.py
@@ -1,6 +1,8 @@
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 def open_data_file():
+    '''returns dictionary with time_tab as float tab ['time'], rr_tab
+    (main score) as float tab ['rr'] and control_sum as int tab ['control_sum']'''
     time_tab = []
     rr_tab = []
     flag_tab = []
@@ -8,35 +10,36 @@ def open_data_file():
 
     for line in file.readlines():
         line = line.strip().split()
+        if line[0] == '#time':
+            continue
         time_tab.append(float(line[0]))
         rr_tab.append(float(line[1]))
         flag_tab.append(int(line[2])) #int or bool it's just a flag
-
-
-    returning_value = []
-    returning_value.append(end_score)
-    returning_value.append(t)
-    returning_value.append(RR)
+    file.close()
+    returning_value = {}
+    returning_value["time"] = time_tab
+    returning_value["rr"] = rr_tab
+    returning_value["control_sum"] = flag_tab
     return returning_value
 
-def count_average(our_tuple):
+def count_average(dict_with_data):
     sum = 0.0
-    for x in range(len(our_tuple)):
-        sum += float(our_tuple[x][1])
-    return sum / len(our_tuple)
+    for value in dict_with_data['rr']:
+        sum += float(value)
+    return sum / len(dict_with_data['rr'])
 
-def find_max(our_tuple):
+def find_max(dict_with_data):
     max = 0.0
-    for x in range(len(our_tuple)):
-        if float(our_tuple[x][1]) > max:
-            max = float(our_tuple[x][1])
+    for value in dict_with_data['rr']:
+        if float(value) > max:
+            max = float(value)
     return max
 
-def find_min(our_tuple):
-    min = find_max(our_tuple)
-    for x in range(len(our_tuple)):
-        if float(our_tuple[x][1]) < min:
-            min = float(our_tuple[x][1])
+def find_min(dict_with_data):
+    min = find_max(dict_with_data)
+    for value in dict_with_data['rr']:
+        if float(value) < min:
+            min = float(value)
     return min
 
 def binning(time_lineup, RR_lineup, binning_interval):
@@ -73,64 +76,55 @@ def sma_algorithm(time_lineup, RR_lineup, sma_length = 3):
         if flag == 2:
             break
         score_after_sma.append((time_lineup[i], float(sum / h_m_samples)))
-        print(time_lineup[i])
-        print("\t" + str(float(sum / h_m_samples)))
         h_m_samples = sma_length
         sum = 0.0
     return score_after_sma
 
 
-def set_range(tup_gen, min_minutes, max_minutes, time_lineup, RR_lineup, binning_interval = 0.0, sma_length = 0):
+def set_range(dict_with_data, min_minutes, max_minutes, binning_interval = 0.0, sma_length = 0):
     i = 0
-    returning_value = []
-    while float(tup_gen[1][i]) <= max_minutes:
-        if float(tup_gen[1][i]) >= min_minutes and float(tup_gen[1][i]) <= max_minutes:
-            time_lineup.append(float(tup_gen[1][i]))
-            RR_lineup.append(float(tup_gen[2][i]))
+    returning_value = {}
+    time_lineup = []
+    rr_lineup = []
+    while float(dict_with_data['time'][i]) <= max_minutes:
+        if float(dict_with_data['time'][i]) >= min_minutes and float(dict_with_data['time'][i]) <= max_minutes:
+            time_lineup.append(float(dict_with_data['time'][i]))
+            rr_lineup.append(float(dict_with_data['rr'][i]))
         i += 1
     if binning_interval > 0.0:
-        returning_value.append(binning(time_lineup, RR_lineup, binning_interval))
+        returning_value['bin_value_range'] = binning(time_lineup, rr_lineup, binning_interval)
     if sma_length > 0:
-        returning_value.append(sma_algorithm(time_lineup, RR_lineup, sma_length))
+        returning_value['sma_val_range'] = sma_algorithm(time_lineup, rr_lineup, sma_length)
     return returning_value
 
-def get_data_from_user(tuple_with_end):
-    returning_value = []
+def get_data_from_user(dict_with_data):
+    returning_value = {}
     minimal_value = float(input("Give minimal time value (in minutes)... "))
     maximal_value = float(input("Give max time value (in minutes)... "))
     bin_val = float(input("Give binning interval... "))
     sma = int(input("Give length of sma... "))
-    maximum_tab_value = find_max(tuple_with_end)
+    maximum_tab_value = find_max(dict_with_data)
     if maximal_value == -1 or maximal_value > maximum_tab_value:
         maximal_value = maximum_tab_value
-    returning_value.append(minimal_value)
-    returning_value.append(maximal_value)
-    returning_value.append(bin_val)
-    returning_value.append(sma)
+    returning_value['min_v'] = minimal_value
+    returning_value['max_v'] = maximal_value
+    returning_value['bin_v'] = bin_val
+    returning_value['sma'] = sma
     return returning_value
 
-tup_gen = open_data_file()
-tuple_with_end_score = tup_gen[0]
-time_lineup = []
-RR_lineup = []
+def start():
+    file_data = open_data_file()
+    user_input = get_data_from_user(file_data)
+    range_to_count = set_range(file_data, user_input['min_v'], user_input['max_v'], user_input['bin_v'], user_input['sma'])
+    print(count_average(file_data))
+    print(find_max(file_data))
+    print(find_min(file_data))
 
-data = get_data_from_user(tuple_with_end_score)
-min_v = data[0]
-max_v = data[1]
-bin_interval = data[2]
-sma_length = data[3]
-
-set_range(tup_gen, min_v, max_v, time_lineup, RR_lineup)
-
-print(count_average(tuple_with_end_score))
-print(find_max(tuple_with_end_score))
-print(find_min(tuple_with_end_score))
-
+start()
+'''
 plt.plot(time_lineup, RR_lineup)
 plt.show()
 
-
-next_show = set_range(tup_gen, min_v, max_v, time_lineup, RR_lineup, bin_interval, sma_length)
 
 time_next = []
 RR_next = []
@@ -150,3 +144,4 @@ for i in range(len(next_show[1])):
 
 plt.plot(time_next, RR_next)
 plt.show()
+'''
